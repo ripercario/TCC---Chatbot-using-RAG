d5ddc77a632cb849bd6e1dd09a7458d415e76a7c
ripercario <ripercario@gmail.com>
2025-05-21 09:39:45 -0300

    Eliminação de PDF

--- create_index.py
+++ create_index.py
@@ -1,4 +1,4 @@
 from app.rag_pipeline import create_vector_store
 
-create_vector_store("data/Book Ajinomoto - Inovações e Resultados 2022 a 2024 - Rev_00.pdf")
+create_vector_store("seu_PDF.pdf")
 
